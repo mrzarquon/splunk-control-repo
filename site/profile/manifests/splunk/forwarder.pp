da1d1bc49e7441615c8d4bd17f79617592f66577
Chris Barker <cbarker@puppet.com>
2018-10-25 16:10:48 -0700

    Add creditcardserver role

--- site/profile/manifests/splunk/forwarder.pp
+++ site/profile/manifests/splunk/forwarder.pp
@@ -1,12 +1,14 @@
 # simple class to deploy the forwarder
 
 class profile::splunk::forwarder (
-  $version = 7.1.3,
-  $build = 51d9cac7b837,
+  $version = '7.1.3',
+  $build = '51d9cac7b837',
+  $server = 'splunk.c.splunk-217321.internal',
 ) {
   class { '::splunk::params':
     version  => $version,
     build    => $build,
+    server   => $server,
     src_root => 'https://download.splunk.com',
   }
 
