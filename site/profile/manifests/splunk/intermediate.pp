098775ff64559fecdb79a58e1048314735aae1e5
Chris Barker <cbarker@puppet.com>
2018-10-25 16:59:20 -0700

    add intermediate forwarder in the right direction this time

--- site/profile/manifests/splunk/intermediate.pp
+++ site/profile/manifests/splunk/intermediate.pp
@@ -2,21 +2,21 @@
 
 class profile::splunk::intermediate {
 
-  @splunkforwarder_output { 'intermediate_compressed':
+  @splunkforwarder_input { 'intermediate_compressed':
     section => 'splunktcp:9997',
     setting => 'compressed',
     value   => 'true',
     tag     => 'splunk_forwarder'
   }
   
-  @splunkforwarder_output { 'intermediate_disabled':
+  @splunkforwarder_input { 'intermediate_disabled':
     section => 'splunktcp:9997',
     setting => 'disabled',
     value   => '0',
     tag     => 'splunk_forwarder'
   }
 
-  @splunkforwarder_output { 'intermediate_connection_host':
+  @splunkforwarder_input { 'intermediate_connection_host':
     section => 'splunktcp:9997',
     setting => 'connection_host',
     value   => 'none',
