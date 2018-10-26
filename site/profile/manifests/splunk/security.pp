6152482c46aa623f1b231ca6b64c2390606ed433
Chris Barker <cbarker@puppet.com>
2018-10-25 17:53:01 -0700

    add windows and security profiles for splunk

---
+++ site/profile/manifests/splunk/security.pp
@@ -0,0 +1,19 @@
+# security monitoring standards
+
+class profile::splunk::security {
+
+  @splunkforwarder_input { 'linux_audit_log-sourcetype':
+    section => 'monitor:///var/log/audit/audit.log',
+    setting => 'sourcetype',
+    value   => 'linux_audit_log',
+    tag     => ['splunk_forwarder', 'security']
+  }
+
+  @splunkforwarder_input { 'linux_secure_log-sourcetype':
+    section => 'monitor:///var/log/secure',
+    setting => 'sourcetype',
+    value   => 'linux_secure_log',
+    tag     => ['splunk_forwarder', 'security']
+  }
+  
+}
---
+++ site/profile/manifests/splunk/winforwarder.pp
@@ -0,0 +1,20 @@
+# simple class to deploy the forwarder
+
+class profile::splunk::forwarder (
+  $version = '7.1.3',
+  $build = '51d9cac7b837',
+  $server = 'splunk.c.splunk-217321.internal',
+) {
+  class { '::splunk::params':
+    version  => $version,
+    build    => $build,
+    server   => $server,
+    src_root => 'https://download.splunk.com',
+  }
+
+  class { '::splunk::forwarder':
+    purge_outputs => true,
+    purge_inputs => true,
+  }
+
+}
