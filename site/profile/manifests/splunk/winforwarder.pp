70bb4b95d08bd79ffd11bf4d0b1814dedf187685
Chris Barker <cbarker@puppet.com>
2018-10-25 17:56:05 -0700

    update

--- site/profile/manifests/splunk/winforwarder.pp
+++ site/profile/manifests/splunk/winforwarder.pp
@@ -1,6 +1,6 @@
 # simple class to deploy the forwarder
 
-class profile::splunk::forwarder (
+class profile::splunk::winforwarder (
   $version = '7.1.3',
   $build = '51d9cac7b837',
   $server = 'splunk.c.splunk-217321.internal',
