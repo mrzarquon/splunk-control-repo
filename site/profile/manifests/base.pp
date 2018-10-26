2db1428fdeaa7b8c191a2752bb41e3552daa2503
Chris Barker <cbarker@puppet.com>
2018-10-25 18:11:41 -0700

    add some failure cases

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -3,6 +3,6 @@ class profile::base {
   #the base profile should include component modules that will be on all nodes
 
   include ::ntp
-  include ::profile::security
+  include ::profile::splunk::security
 
 }
