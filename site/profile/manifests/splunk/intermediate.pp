c9068cb553a945d1b51dd93f0ae38c2080b1e84b
Chris Barker <cbarker@puppet.com>
2018-10-25 19:08:58 -0700

    add intermediate server logging

--- site/profile/manifests/splunk/intermediate.pp
+++ site/profile/manifests/splunk/intermediate.pp
@@ -19,8 +19,23 @@ class profile::splunk::intermediate {
   @splunkforwarder_input { 'intermediate_connection_host':
     section => 'splunktcp:9997',
     setting => 'connection_host',
-    value   => 'none',
+    value   => 'splunk.c.splunk-217321.internal',
     tag     => 'splunk_forwarder'
   }
+
+  @splunkforwarder_input { 'splunk_logs-sourcetype':
+    section => "monitor:///opt/splunkforwarder/var/log/splunk/splunkd.log",
+    setting => 'sourcetype',
+    value   => 'splunk_logs',
+    tag     => 'splunk_forwarder'
+  }
+
+  @splunkforwarder_input { 'splunk_errors-sourcetype':
+    section => "monitor:///opt/splunkforwarder/var/log/splunk/splunkd_stderr.log",
+    setting => 'sourcetype',
+    value   => 'splunk_errors',
+    tag     => 'splunk_forwarder'
+  }
+
 
 }
