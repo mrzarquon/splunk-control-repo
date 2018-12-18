f6dad65459777f1a8ef3430c55af1b15d859787b
Chris Barker <cbarker@puppet.com>
2018-12-18 14:06:23 +0000

    add console and orch services logging

--- site/profile/manifests/splunk/puppetinfrastructure.pp
+++ site/profile/manifests/splunk/puppetinfrastructure.pp
@@ -4,6 +4,7 @@
 # /var/log/puppetlabs/puppetdb/puppetdb.log
 # /var/log/puppetlabs/puppetdb/puppetdb-access.log
 
+
 class profile::splunk::puppetinfrastructure {
 
   @splunkforwarder_input { 'puppetserver-access_log-sourcetype':
@@ -26,4 +27,26 @@ class profile::splunk::puppetinfrastructure {
     value   => 'puppetdb-access_log',
     tag     => 'splunk_forwarder',
   }
+
+  @splunkforwarder_input { 'pe-orchestration-services_log-sourcetype':
+    section => "monitor:///var/log/puppetlabs/orchestration-services/orchestration-services.log",
+    setting => 'sourcetype',
+    value   => 'pe-orchestration-services_log',
+    tag     => 'splunk_forwarder',
+  }
+
+  @splunkforwarder_input { 'pe-console-services-access_log-sourcetype':
+    section => "monitor:///var/log/puppetlabs/console-services/console-services-access.log",
+    setting => 'sourcetype',
+    value   => 'pe-console-services-access_log',
+    tag     => 'splunk_forwarder',
+  }
+
+  @splunkforwarder_input { 'pe-console-services-api-access_log-sourcetype':
+    section => "monitor:///var/log/puppetlabs/console-services/console-services-api-access.log",
+    setting => 'sourcetype',
+    value   => 'pe-console-services-api-access_log',
+    tag     => 'splunk_forwarder',
+  }
+
 }
